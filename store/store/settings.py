91baef27a3c9559d85b40a19646426719708a730
Aswin-ashi <asrjk2002@gmail.com>
2025-12-19 16:21:18 +0530

    initial commit

--- store/store/settings.py
+++ store/store/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-1#067lneq7ecm4(3le&9bu6aou8)tgbj^jvb7!fmj6opuz$_+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['aswinashi.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
