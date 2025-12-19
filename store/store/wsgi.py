831a0c2506719dfe343bac5ba9a9cb0830b84366
Aswin-ashi <asrjk2002@gmail.com>
2025-12-19 17:15:36 +0530

    initial commit

--- store/store/wsgi.py
+++ store/store/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+
+# application = get_wsgi_application()
+
 import os
+import sys
+
+# Add your project directory to the sys.path
+path = '/home/aswinashi/Online-Kudumbasree-store'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'store.settings' # Adjust if your settings are elsewhere
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
-
 application = get_wsgi_application()
