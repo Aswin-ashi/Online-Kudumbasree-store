2bd8999a3176aac26ec6bdf96119c780b75df5a9
Aswin-ashi <asrjk2002@gmail.com>
2026-09-08 12:35:00 +0530

    Fix Vercel static routing hijack and enable WhiteNoise finders

--- store/settings.py
+++ store/settings.py
@@ -140,8 +140,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
